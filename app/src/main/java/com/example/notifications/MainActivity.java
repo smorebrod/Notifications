333f25798d258aa00f8dd8f74d3b4e9f5e7a8c9d
Alexander <Alexander_kastbjerg_larsen@yahoo.dk>
2019-12-02 22:38:16 +0100

    Intent

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -10,6 +10,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,28 +38,56 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        Intent activityIntent = new Intent(this,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_format)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
                 .build();
 
         notificationManager.notify(1, notification);
     }
 
     public void sendOnChannel2(View v) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+        final int progressMax = 100;
+
+
+        final NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_message)
-                .setContentTitle(title)
-                .setContentText(message)
+                .setContentTitle("Download")
+                .setContentText("Download in progress")
                 .setPriority(NotificationCompat.PRIORITY_LOW)
-                .build();
+                .setOngoing(true)
+                .setOnlyAlertOnce(true)
+                .setProgress(progressMax,0,false);
 
-        notificationManager.notify(2, notification);
+
+        notificationManager.notify(2, notification.build());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2000);
+                for (int progress = 0; progress <= progressMax; progress += 10){
+                    notification.setProgress(progressMax, progress, false);
+                    notificationManager.notify(2,notification.build());
+                    SystemClock.sleep(1000);
+                }
+
+                notification.setContentText("Download finished")
+                        .setProgress(0,0,false)
+                .setOngoing(false);
+                notificationManager.notify(2,notification.build());
+
+            }
+        }).start();
+
+
     }
 }
